2c61836a8bd7d00ab94e50630557bf844c750d43
FelipeSSac <felipe_adoubs@outlook.com>
2021-06-20 22:11:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
